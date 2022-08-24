3950428c8f151371dfb0dd952d4f206bf725206e
Tainá <111010140+tainags@users.noreply.github.com>
2022-08-24 21:01:42 +0000

    Aula 3

--- Modelos/Aluno.cs
+++ Modelos/Aluno.cs
@@ -1,12 +1,39 @@
 namespace Modelos{
 
   class Aluno{
+
+    public string nome;
     public string matricula;
     public int periodo;
+   
+    public Aluno () : this(0) {
+      
+    }
+    
+      public Aluno(int periodo) {
+      if (periodo > 0) {
+        this.periodo = periodo;
+      } else {
+        this.periodo = 1;      
+      }
+      
+    }
 
     public bool Matricular(Disciplina disciplina){
        return this.periodo == disciplina.semestre;
     }
+
+    public Aluno(string matricula, string nome) : this() {
+      this.matricula = matricula;
+      this.nome = nome;              
+   }
+    
+   
+    public Aluno(string matricula, string nome, int periodo) : this(periodo) {
+      this.matricula = matricula;
+      this.nome = nome;
+               
+   }
   }
 }
 
--- Modelos/Disciplina.cs
+++ Modelos/Disciplina.cs
@@ -2,7 +2,22 @@ namespace Modelos{
 
   class Disciplina{
     
-     public string nome;
+    public string nome;
     public int semestre;
+    public Disciplina (){
+      
+    }
+
+    public Disciplina(int semestre){
+      if (semestre < 1) {
+          this.semestre = 1;      
+      } else if (semestre > 10) {
+        this.semestre = 10;
+      }
+        else{
+          this.semestre = semestre;
+        }
+    }
+    
   }
 }
